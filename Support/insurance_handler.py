26934042abcaa50bf4944cb566cf47b6a6dc301f
Konstantin <polovnikov.k.v@gmail.com>
2024-06-08 21:07:30 +0700

    fix requiremernts.txt: q

--- Support/insurance_handler.py
+++ Support/insurance_handler.py
@@ -5,5 +5,4 @@ async def insurance_handler(client, message):
     user_id = message.from_user.id
     lang = client.user_data[user_id]["lang"]
     response = messages[lang]["insurance_about"]
-    print("log")
     await client.send_message(user_id, response, disable_web_page_preview=True)
